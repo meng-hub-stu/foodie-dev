bd1e0c5c6e69271e200093423817fd2caa6c40da
meng-hub-stu <email@example.com>
2022-01-08 12:46:32 +0800

    feat(foodie-dev): elasticsearch

--- foodie-dev-api/src/main/java/com/imooc/Application.java
+++ foodie-dev-api/src/main/java/com/imooc/Application.java
@@ -6,14 +6,14 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+//import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * @author Mengdl
  * @date 2021/11/01
  */
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication/*(exclude = {SecurityAutoConfiguration.class})*/
 // 扫描 mybatis 通用 mapper 所在的包
 @MapperScan(basePackages = {"com.imooc.mapper"})
 @ComponentScan(basePackages = {"com.imooc", "org.n3r.idworker"})
